1c6abb4f75760263c198479bb9dd1bae5290e9cd
lizp <lizp@lhzdtech.com>
2018-07-13 17:26:38 +0800

    添加线程安全

---
+++ 线程基础/线程基础下/ThreadOneVC.m
@@ -0,0 +1,37 @@
+//
+//  ThreadOneVC.m
+//  线程基础
+//
+//  Created by 李泽平 on 2018/7/13.
+//  Copyright © 2018年 李泽平. All rights reserved.
+//
+
+#import "ThreadOneVC.h"
+
+@interface ThreadOneVC ()
+
+@end
+
+@implementation ThreadOneVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ 线程基础/线程基础下/ThreadTwoVC.h
@@ -0,0 +1,13 @@
+//
+//  ThreadTwoVC.h
+//  线程基础
+//
+//  Created by 李泽平 on 2018/7/13.
+//  Copyright © 2018年 李泽平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThreadTwoVC : UIViewController
+
+@end
